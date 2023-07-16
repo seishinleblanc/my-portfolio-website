55e312785ee398ed32b0d1a4af6e23b9de57889f
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-07-16 02:30:16 -0500

    Added animations and finished the basic website design.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -18,6 +24,12 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
+            // active sections for animation on scroll 
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this 
+        else {
+            sec.classList.remove('show-animate');
         }
     });
 
@@ -25,4 +37,13 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
